bf8e8ce586d0c47026449d51e057a0977956e7aa
aditapdiya <122151751+aditapdiya@users.noreply.github.com>
2023-01-31 17:01:38 +0530

    Update rmv.py

--- rmv.py
+++ rmv.py
@@ -1,12 +1,23 @@
 import cv2
 import numpy as np
-for i in range( 0, 21):
+from pdf2jpg import pdf2jpg
+def sort():
+    inputpath = r"INSERT THE PATHH OF FILE"
+    outputpath = r" OUTPUT PATH FILE NAME"
+    result = pdf2jpg.convert_pdf2jpg(inputpath,outputpath, pages="ALL")
+    rem()
 
-    img = cv2.imread("F:\\office\\Python-Remove-Watermark-master\\output\\image1.png\\CW-01.pdf_dir\\"+str(i)+"_CW-01.pdf.jpg")
+
+def rem():
+ for i in range( 0, 21):
+
+    img = cv2.imread("IMAGE PATH ")
 
     alpha = 2.0
     beta = -170
     new = alpha * img + beta
     new = np.clip(new, 0, 255).astype(np.uint8)
 
-    cv2.imwrite("F:\\office\\Python-Remove-Watermark-master\\output\\cleaned"+str(i)+".png",new)
+    cv2.imwrite("OUTUT IMAGE SAVE PATH",new)
+
+sort()
